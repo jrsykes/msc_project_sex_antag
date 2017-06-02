312755dfdd8703c201ccd9658dec8be93a67352f
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-02 14:26:35 +0100

    wget update

---
+++ scripts/one_download_and_qc_wget.sh
@@ -0,0 +1,59 @@
+#!/bin/bash​
+#$ -S /bin/bash
+#$ -q main.q
+#$ -pe smp 4
+#$ -l h_vmem=10G
+#$ -cwd
+
+#Mdify the species name variable
+#For every fastq file to be downloaded, unhash the necesary lines and modify the vlaues for the SSR# and sex variables.
+#IMPORTANT. Go to fastqc qsub script at /data/projects/lross_ssa/scripts/fastqc.sh and edit acordingly before running this script.
+
+species=rhipicephalus_pulchellus
+mkdir /data/projects/lross_ssa/raw/$species
+mkdir /data/projects/lross_ssa/raw/$species/male
+mkdir /data/projects/lross_ssa/raw/$species/female
+
+#mkdir /data/projects/lross_ssa/analyses/$species
+#mkdir /data/projects/lross_ssa/analyses/$species/fastqc
+#mkdir /data/projects/lross_ssa/analyses/$species/fastqc/male
+#mkdir /data/projects/lross_ssa/analyses/$species/fastqc/female
+    
+
+SRR=SRR2028039
+sex=male
+echo -n $SRR | wc -c > SRRn.txt
+read SRRn < SRRn.txt
+
+if [ $SRRn -eq 9 ]
+then
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/$SRR/$SRR\_1.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/$SRR/$SRR\_2.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+fi
+
+if [ $SRRn -eq 10 ]
+then
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/00${SRR:9:11}/$SRR/$SRR\_1.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/00${SRR:9:11}/$SRR/$SRR\_2.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+fi
+
+if [ $SRRn -eq 11 ]
+then
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/0${SRR:9:12}/$SRR/$SRR\_1.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/0${SRR:9:12}/$SRR/$SRR\_2.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+fi
+
+if [ $SRRn -eq 12 ]
+then
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/${SRR:9:13}/$SRR/$SRR\_1.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${SRR:0:6}/${SRR:9:13}/$SRR/$SRR\_2.fastq.gz > /data/projects/lross_ssa/raw/$species/$sex
+fi
+
+gunzip /data/projects/lross_ssa/raw/$species/male/*
+gunzip /data/projects/lross_ssa/raw/$species/female/*
+
+cp /data/projects/lross_ssa/scripts/*.sh.* /data/projects/lross_ssa/raw/$species
+
+#qsub /data/projects/lross_ssa/scripts/fastqc.sh
+
+
