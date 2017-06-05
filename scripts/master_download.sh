935c3600e4fa24be7891006ff23cda25c9476ef0
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-05 01:24:00 +0100

    .

--- scripts/master_download.sh
+++ scripts/master_download.sh
@@ -2,21 +2,21 @@
 
 species=$1
 
-mkdir /data/projects/lross_ssa/raw/$species
-mkdir /data/projects/lross_ssa/raw/$species/male
-mkdir /data/projects/lross_ssa/raw/$species/female
-
-mkdir /data/projects/lross_ssa/analyses/$species
-mkdir /data/projects/lross_ssa/analyses/$species/fastqc
-mkdir /data/projects/lross_ssa/analyses/$species/fastqc2
-
-mkdir /data/projects/lross_ssa/analyses/$species/trinity
-mkdir /data/projects/lross_ssa/analyses/$species/busco
-mkdir /data/projects/lross_ssa/analyses/$species/kallisto
-
-mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic
-mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic/male
-mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic/female
+#mkdir /data/projects/lross_ssa/raw/$species
+#mkdir /data/projects/lross_ssa/raw/$species/male
+#mkdir /data/projects/lross_ssa/raw/$species/female
+#
+#mkdir /data/projects/lross_ssa/analyses/$species
+#mkdir /data/projects/lross_ssa/analyses/$species/fastqc
+#mkdir /data/projects/lross_ssa/analyses/$species/fastqc2
+#
+#mkdir /data/projects/lross_ssa/analyses/$species/trinity
+#mkdir /data/projects/lross_ssa/analyses/$species/busco
+#mkdir /data/projects/lross_ssa/analyses/$species/kallisto
+#
+#mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic
+#mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic/male
+#mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic/female
 
 
 ## one = species, two = SRR, three = sex, four = paired or single end mode
