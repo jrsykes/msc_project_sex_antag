3df41b2f3111c623ceea7f3dad79205842f193a7
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-05 14:26:15 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -2,6 +2,9 @@
 
 script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
-qsub $script bemisia_tabaci SRR5086009 female paired 
-qsub $script bemisia_tabaci SRR5086010 male paired
-qsub $script bemisia_tabaci SRR1523522 female paired
+### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
+
+qsub $script ericerus_pela SRR1027689 female single 
+qsub $script ericerus_pela SRR1027688 female single
+qsub $script ericerus_pela SRR1027687 female single
+qsub $script ericerus_pela SRR1027692 male single 
