ba13035fe25a36bdbb087f0b155a98f10d3c4032
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-05 21:19:46 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,7 +4,7 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script ericerus_pela SRR1027689 female single 
-qsub $script ericerus_pela SRR1027688 female single
-qsub $script ericerus_pela SRR1027687 female single
-qsub $script ericerus_pela SRR1027692 male single 
+qsub $script pachypsylla_venusta SRR1824579 female paired 
+qsub $script pachypsylla_venusta SRR1824580 male paired
+#qsub $script  SRR1027687 female single
+#qsub $script  SRR1027692 male single 
