ebcf79ad77aeaf24cf593aca972b0abe9b4d3e64
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-08 17:00:28 +0100

    .

--- scripts/map.sh
+++ scripts/map.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -q main.q
-#$ -pe smp 4
-#$ -l h_vmem=20G
+#$ -pe smp 16
+#$ -l h_vmem=40G
 #$ -wd /data/projects/lross_ssa/analyses/temp_out/map
 
 SPECIES=$1
-SRR=$2
-SEX=$3
-MODE=$4
+MODE=$2
+
 
 if [ $MODE == 'paired' ]
-	then
+then
+mkdir /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined
+ln -s /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/*1.fq /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined
+ln -s /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/*1.fq /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined
 
-/exports/software/kallisto/kallisto_linux-v0.43.1/kallisto quant -t 16 -i /data/projects/lross_ssa/analyses/$SPECIES/kallisto/$SPECIES\_indexed.idx -o /data/projects/lross_ssa/analyses/$SPECIES/kallisto/$SRR -b 100 /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/$SEX/$SRR\_1.fq /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/$SEX/$SRR\_2.fq
+ln -s /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/*2.fq /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined
+ln -s /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/*2.fq /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined
+INPUT=$(for file in $(ls -1 /data/projects/lross_ssa/analyses/$SPECIES/kallisto/combined); do readlink -f $file ;done | paste -)
 
+
+/exports/software/kallisto/kallisto_linux-v0.43.1/kallisto quant -t 16 -i /data/projects/lross_ssa/analyses/$SPECIES/kallisto/$SPECIES\_indexed.idx -o /data/projects/lross_ssa/analyses/$SPECIES/kallisto/ -b 100 $INPUT
+rm -rf /data/projects/lross_ssa/analyses/bemisia_tabaci/kallisto/combined
 fi
 
 if [ $MODE == 'single' ]
