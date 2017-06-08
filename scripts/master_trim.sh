03d22f58758c77f6c56c7999d3b889ac693a4914
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-09 00:37:38 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,7 +4,11 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script lygus_hesperus SRR925995 male paired 
-qsub $script lygus_hesperus SRR925994 male paired
-qsub $script lygus_hesperus SRR925993 female paired
-qsub $script lygus_hesperus SRR925992 female paired
+qsub $script teleopsis_dalmanni SRR1738676 male paired
+qsub $script teleopsis_dalmanni SRR1738677 female paired
+qsub $script teleopsis_dalmanni SRR1184536 female paired
+qsub $script teleopsis_dalmanni SRR1184535 female paired
+qsub $script teleopsis_dalmanni SRR1184534 female paired
+qsub $script teleopsis_dalmanni SRR1184541 male paired
+qsub $script teleopsis_dalmanni SRR1184537 male paired
+qsub $script teleopsis_dalmanni SRR1184533 male paired
