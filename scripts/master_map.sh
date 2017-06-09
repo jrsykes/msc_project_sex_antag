e0a952f5b64cbf4613cf9d4b4358791f0d62972e
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-09 14:31:21 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -2,10 +2,10 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script pachypsylla_venusta SRR1824579 female paired
-qsub $script pachypsylla_venusta SRR1824580 male paired
-#qsub $script 
-#qsub $script 
+qsub $script lygus_hesperus SRR925995 male paired
+qsub $script lygus_hesperus SRR925994 male paired
+qsub $script lygus_hesperus SRR925993 female paired
+qsub $script lygus_hesperus SRR925992 female paired
 #qsub $script 
 #qsub $script 
 #qsub $script 
