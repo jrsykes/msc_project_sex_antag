2dd832c295c63be0de92ed87cdb726dd0581db43
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-10 00:07:41 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,12 +4,13 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script athalia_rosae SRR1842142 male paired
-qsub $script athalia_rosae SRR1842141 male paired
-qsub $script athalia_rosae SRR1842140 female paired
-qsub $script athalia_rosae SRR1842139 female paired
+qsub $script ceratosolen_solmsi SRR974925 female paired 
+qsub $script ceratosolen_solmsi SRR974929 female paired
+qsub $script ceratosolen_solmsi SRR974927 female paired
+qsub $script ceratosolen_solmsi SRR974928 male paired
+qsub $script ceratosolen_solmsi SRR974926 male paired
 
-#qsub $script phlebotomus_perniciosus SRR2075599 female paired
+
 #qsub $script phlebotomus_perniciosus SRR2074672 female paired
 #qsub $script teleopsis_dalmanni SRR1184537 male paired
 #qsub $script teleopsis_dalmanni SRR1184533 male paired
