0e03426b6aedef768f2fbe0a7a5fc0d7e4983cf3
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-09 16:56:29 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,11 +4,13 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script teleopsis_dalmanni SRR1738676 male paired
-qsub $script teleopsis_dalmanni SRR1738677 female paired
-qsub $script teleopsis_dalmanni SRR1184536 female paired
-qsub $script teleopsis_dalmanni SRR1184535 female paired
-qsub $script teleopsis_dalmanni SRR1184534 female paired
-qsub $script teleopsis_dalmanni SRR1184541 male paired
-qsub $script teleopsis_dalmanni SRR1184537 male paired
-qsub $script teleopsis_dalmanni SRR1184533 male paired
+qsub $script phlebotomus_perniciosus SRR2075602 male paired
+qsub $script phlebotomus_perniciosus SRR2075601 male paired
+qsub $script phlebotomus_perniciosus SRR2074666 male paired
+qsub $script phlebotomus_perniciosus SRR2075600 female paired
+qsub $script phlebotomus_perniciosus SRR2075599 female paired
+qsub $script phlebotomus_perniciosus SRR2074672 female paired
+
+
+#qsub $script teleopsis_dalmanni SRR1184537 male paired
+#qsub $script teleopsis_dalmanni SRR1184533 male paired
