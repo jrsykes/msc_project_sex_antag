6c4903efe3db1a408ce4a8ae2af22b4046abe3ef
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-09 22:55:47 +0100

    .

--- scripts/master_download.sh
+++ scripts/master_download.sh
@@ -30,10 +30,10 @@ mkdir /data/projects/lross_ssa/analyses/$species/trimmomatic/female
 script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/download_qc.sh
 
  
-qsub $script ceratosolen_solmsi SRR974925 female paired 
-qsub $script ceratosolen_solmsi SRR974929 female paired
-#qsub $script ceratosolen_solmsi SRR974927 female paired
-#qsub $script ceratosolen_solmsi SRR974928 male paired
+#qsub $script ceratosolen_solmsi SRR974925 female paired 
+#qsub $script ceratosolen_solmsi SRR974929 female paired
+qsub $script ceratosolen_solmsi SRR974927 female paired
+qsub $script ceratosolen_solmsi SRR974928 male paired
 #qsub $script ceratosolen_solmsi SRR974926 male paired
 
 
