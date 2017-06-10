98847ceddbe55d4b445989332dae4997d8c0fc81
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-10 20:15:17 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,11 +4,12 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script agrilus_planipennis SRR1791267 female paired
-qsub $script agrilus_planipennis SRR1791269 male paired
+qsub $script ericerus_pela SRR1027689 female single
+qsub $script ericerus_pela SRR1027688 female single
+qsub $script ericerus_pela SRR1027687 female single
+qsub $script ericerus_pela SRR1027692 male single
 
-#qsub $script ceratosolen_solmsi SRR974927 female paired
-#qsub $script ceratosolen_solmsi SRR974928 male paired
+
 #qsub $script ceratosolen_solmsi SRR974926 male paired
 #qsub $script frankliniella_occidentalis SRR1826956 female paired
 #qsub $script frankliniella_occidentalis SRR1826954 male paired
