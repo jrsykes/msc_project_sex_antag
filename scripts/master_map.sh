dc1270579ab1809305b2b42a2968bf579fbf7673
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-10 12:31:56 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -2,27 +2,28 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script mayetiola_destructor SRR1738672 female paired
-qsub $script mayetiola_destructor SRR1738673 male paired
-qsub $script mayetiola_destructor SRR1239355 male paired
-qsub $script mayetiola_destructor SRR1239356 female paired
-qsub $script mayetiola_destructor SRR1239358 male paired
-qsub $script mayetiola_destructor SRR1239354 female paired
-qsub $script teleopsis_dalmanni SRR1738676 male paired
-qsub $script teleopsis_dalmanni SRR1738677 female paired
-qsub $script teleopsis_dalmanni SRR1184536 female paired
-qsub $script teleopsis_dalmanni SRR1184535 female paired
-qsub $script teleopsis_dalmanni SRR1184534 female paired
-#qsub $script teleopsis_dalmanni SRR1184541 male paired
-#qsub $script teleopsis_dalmanni SRR1184537 male paired
-#qsub $script teleopsis_dalmanni SRR1184533 male paired
-#qsub $script phlebotomus_perniciosus SRR2075602 male paired
-#qsub $script phlebotomus_perniciosus SRR2075601 male paired
-#qsub $script phlebotomus_perniciosus SRR2074666 male paired
-#qsub $script phlebotomus_perniciosus SRR2075600 female paired
-#qsub $script phlebotomus_perniciosus SRR2075599 female paired
-#qsub $script phlebotomus_perniciosus SRR2074672 female paired
-#qsub $script athalia_rosae SRR1842142 male paired
-#qsub $script athalia_rosae SRR1842141 male paired
-#qsub $script athalia_rosae SRR1842140 female paired
-#qsub $script athalia_rosae SRR1842139 female paired
+#qsub $script mayetiola_destructor SRR1738672 female paired
+#qsub $script mayetiola_destructor SRR1738673 male paired
+#qsub $script mayetiola_destructor SRR1239355 male paired
+#qsub $script mayetiola_destructor SRR1239356 female paired
+#qsub $script mayetiola_destructor SRR1239358 male paired
+#qsub $script mayetiola_destructor SRR1239354 female paired
+#qsub $script teleopsis_dalmanni SRR1738676 male paired
+#qsub $script teleopsis_dalmanni SRR1738677 female paired
+#qsub $script teleopsis_dalmanni SRR1184536 female paired
+#qsub $script teleopsis_dalmanni SRR1184535 female paired
+#qsub $script teleopsis_dalmanni SRR1184534 female paired
+
+qsub $script teleopsis_dalmanni SRR1184541 male paired
+qsub $script teleopsis_dalmanni SRR1184537 male paired
+qsub $script teleopsis_dalmanni SRR1184533 male paired
+qsub $script phlebotomus_perniciosus SRR2075602 male paired
+qsub $script phlebotomus_perniciosus SRR2075601 male paired
+qsub $script phlebotomus_perniciosus SRR2074666 male paired
+qsub $script phlebotomus_perniciosus SRR2075600 female paired
+qsub $script phlebotomus_perniciosus SRR2075599 female paired
+qsub $script phlebotomus_perniciosus SRR2074672 female paired
+qsub $script athalia_rosae SRR1842142 male paired
+qsub $script athalia_rosae SRR1842141 male paired
+qsub $script athalia_rosae SRR1842140 female paired
+qsub $script athalia_rosae SRR1842139 female paired
