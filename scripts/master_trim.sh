c4ae223570f442f5363666c7761c8d5b002bdc62
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-10 23:13:47 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,13 +4,20 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script ericerus_pela SRR1027689 female single
-qsub $script ericerus_pela SRR1027688 female single
-qsub $script ericerus_pela SRR1027687 female single
-qsub $script ericerus_pela SRR1027692 male single
-
-
-#qsub $script ceratosolen_solmsi SRR974926 male paired
-#qsub $script frankliniella_occidentalis SRR1826956 female paired
-#qsub $script frankliniella_occidentalis SRR1826954 male paired
-#qsub $script phlebotomus_perniciosus SRR2074672 female paired
+qsub $script nasonia_giraulti SRR2773798 female single
+qsub $script nasonia_giraulti SRR2773797 female single
+qsub $script nasonia_giraulti SRR2773796 female single
+qsub $script nasonia_giraulti SRR2773795 female single
+qsub $script nasonia_giraulti SRR2773794 female single
+qsub $script nasonia_giraulti SRR1566030 female single
+qsub $script nasonia_giraulti SRR1566029 female single
+qsub $script nasonia_giraulti SRR1566028 female single
+#qsub $script nasonia_giraulti SRR1566024 female single
+#qsub $script nasonia_giraulti SRR1566023 female single
+#qsub $script nasonia_giraulti SRR1566022 female single
+#qsub $script nasonia_giraulti SRR1566033 male single
+#qsub $script nasonia_giraulti SRR1566032 male single
+#qsub $script nasonia_giraulti SRR1566031 male single
+#qsub $script nasonia_giraulti SRR1566027 male single
+#qsub $script nasonia_giraulti SRR1566026 male single
+#qsub $script nasonia_giraulti SRR1566025 male single
