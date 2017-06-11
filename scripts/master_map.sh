bccb63ef6b3a9ef95ae117f79bed6438d2a28ce1
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-11 11:37:09 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -2,11 +2,13 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script ceratosolen_solmsi SRR974925 female paired
-qsub $script ceratosolen_solmsi SRR974929 female paired
-qsub $script ceratosolen_solmsi SRR974927 female paired
-qsub $script ceratosolen_solmsi SRR974928 male paired
-qsub $script ceratosolen_solmsi SRR974926 male paired
+qsub $script frankliniella_occidentalis SRR1826956 female paired
+qsub $script frankliniella_occidentalis SRR1826954 male paired
+
+
+#qsub $script 
+#qsub $script 
+#qsub $script 
 
 #qsub $script 
 #qsub $script 
