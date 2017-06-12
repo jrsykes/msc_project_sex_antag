720a1b4e890dbb9d5561bc3d860cf3456799210c
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-12 21:47:35 +0100

    ..

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -4,10 +4,7 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ##### IMPORTANT. REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ########### for most this should be -l 100 and -s 20
 
-qsub $script ericerus_pela SRR1027689 female single
-qsub $script ericerus_pela SRR1027688 female single
-qsub $script ericerus_pela SRR1027687 female single
-qsub $script ericerus_pela SRR1027692 male single
+qsub $script nasonia_giraulti SRR2773798 female single 
 
 
 #qsub $script 
