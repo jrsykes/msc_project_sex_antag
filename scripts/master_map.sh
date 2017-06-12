f5e5105f4530149edfef4fb9eec82809215c6b12
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-12 11:13:24 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -4,23 +4,24 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ##### IMPORTANT. REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ###########
 
-qsub $script nasonia_giraulti SRR2773799 female single 
-qsub $script nasonia_giraulti SRR2773797 female single 
-qsub $script nasonia_giraulti SRR2773796 female single 
-qsub $script nasonia_giraulti SRR2773795 female single 
-qsub $script nasonia_giraulti SRR2773794 female single 
-qsub $script nasonia_giraulti SRR1566030 female single 
-qsub $script nasonia_giraulti SRR1566029 female single 
-qsub $script nasonia_giraulti SRR1566028 female single 
-qsub $script nasonia_giraulti SRR1566024 female single 
-qsub $script nasonia_giraulti SRR1566023 female single 
-qsub $script nasonia_giraulti SRR1566022 female single 
-qsub $script nasonia_giraulti SRR1566033 male single 
-qsub $script nasonia_giraulti SRR1566032 male single 
-qsub $script nasonia_giraulti SRR1566031 male single 
-qsub $script nasonia_giraulti SRR1566027 male single 
-qsub $script nasonia_giraulti SRR1566026 male single 
-qsub $script nasonia_giraulti SRR1566025 male single
+qsub $script amblyomma_americanum SRR4416255 male single
+qsub $script amblyomma_americanum SRR4416253 male single
+qsub $script amblyomma_americanum SRR4416251 male single
+qsub $script amblyomma_americanum SRR4416254 female single
+qsub $script amblyomma_americanum SRR4416252 female single
+qsub $script amblyomma_americanum SRR4416250 female single
+
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
+#qsub $script 
 
 
 #qsub $script 
