2d965d6e5c30b61b0046c11099c750ddf749fc57
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-12 12:39:54 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -2,7 +2,7 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-##### IMPORTANT. REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ###########
+##### IMPORTANT. REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ########### for most this should be -l 100 and -s 20
 
 qsub $script amblyomma_americanum SRR4416255 male single
 qsub $script amblyomma_americanum SRR4416253 male single
