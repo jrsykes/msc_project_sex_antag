bfdcc7b34f696fa50fb47b6d412a279223c567b6
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-12 14:48:47 +0100

    ..

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -4,14 +4,15 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script nasonia_giraulti SRR2773798 female single
-qsub $script nasonia_giraulti SRR2773797 female single
-qsub $script nasonia_giraulti SRR2773796 female single
-qsub $script nasonia_giraulti SRR2773795 female single
-qsub $script nasonia_giraulti SRR2773794 female single
-qsub $script nasonia_giraulti SRR1566030 female single
-qsub $script nasonia_giraulti SRR1566029 female single
-qsub $script nasonia_giraulti SRR1566028 female single
+qsub $script leptinotarsa_decemlineata SRR1827566 male paired
+qsub $script leptinotarsa_decemlineata SRR1827565 female paired
+
+#qsub $script nasonia_giraulti SRR2773796 female single
+#qsub $script nasonia_giraulti SRR2773795 female single
+#qsub $script nasonia_giraulti SRR2773794 female single
+#qsub $script nasonia_giraulti SRR1566030 female single
+#qsub $script nasonia_giraulti SRR1566029 female single
+#qsub $script nasonia_giraulti SRR1566028 female single
 
 #qsub $script nasonia_giraulti SRR1566024 female single
 #qsub $script nasonia_giraulti SRR1566023 female single
