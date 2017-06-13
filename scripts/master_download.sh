12aa14e4b277a39183a3cc32017fe5a4e2a36496
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-13 18:28:52 +0100

    .

--- scripts/master_download.sh
+++ scripts/master_download.sh
@@ -42,10 +42,10 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/download_q
 #qsub $script rhizoglyphus_robini SRR3934326 male single
 #qsub $script rhizoglyphus_robini SRR3934327 male single
 #qsub $script rhizoglyphus_robini SRR3934328 male single
-qsub $script rhizoglyphus_robini SRR3934329 male single
-qsub $script rhizoglyphus_robini SRR3934330 male single
-#qsub $script rhizoglyphus_robini SRR3934331 male single
-#qsub $script rhizoglyphus_robini SRR949074 male paired
+#qsub $script rhizoglyphus_robini SRR3934329 male single
+#qsub $script rhizoglyphus_robini SRR3934330 male single
+qsub $script rhizoglyphus_robini SRR3934331 male single
+qsub $script rhizoglyphus_robini SRR949074 male paired
 #qsub $script rhizoglyphus_robini SRR949075 male paired
 #qsub $script rhizoglyphus_robini SRR3934338 male single
 #qsub $script rhizoglyphus_robini SRR3934339 male single
