f0b5db76189ade60759e6521aee75a1ab23e595d
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-14 00:13:21 +0100

    .

--- scripts/master_trim.sh
+++ scripts/master_trim.sh
@@ -5,22 +5,25 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/trim.sh
  1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
 
-qsub $script rhizoglyphus_robini SRR3934324 female single
-qsub $script rhizoglyphus_robini SRR3934325 female single
-qsub $script rhizoglyphus_robini SRR3934332 female single
-qsub $script rhizoglyphus_robini SRR3934333 female single
-qsub $script rhizoglyphus_robini SRR3934334 female single
-qsub $script rhizoglyphus_robini SRR3934335 female single
-qsub $script rhizoglyphus_robini SRR3934336 female single
-qsub $script rhizoglyphus_robini SRR3934337 female single
-qsub $script rhizoglyphus_robini SRR946953 female paired
-qsub $script rhizoglyphus_robini SRR3934326 male single
-qsub $script rhizoglyphus_robini SRR3934327 male single
-qsub $script rhizoglyphus_robini SRR3934328 male single
-qsub $script rhizoglyphus_robini SRR3934329 male single
-qsub $script rhizoglyphus_robini SRR3934330 male single
-qsub $script rhizoglyphus_robini SRR3934331 male single
-qsub $script rhizoglyphus_robini SRR949074 male paired
-qsub $script rhizoglyphus_robini SRR949075 male paired
-qsub $script rhizoglyphus_robini SRR3934338 male single
-qsub $script rhizoglyphus_robini SRR3934339 male single
+qsub $script tetranychus_urticae SRR1582619 female paired
+qsub $script tetranychus_urticae SRR1582618 female paired
+qsub $script tetranychus_urticae SRR1582616 female paired
+qsub $script tetranychus_urticae SRR1582617 female paired
+
+
+
+#qsub $script rhizoglyphus_robini SRR3934334 female single
+#qsub $script rhizoglyphus_robini SRR3934335 female single
+#qsub $script rhizoglyphus_robini SRR3934336 female single
+#qsub $script rhizoglyphus_robini SRR3934337 female single
+#qsub $script rhizoglyphus_robini SRR946953 female paired
+#qsub $script rhizoglyphus_robini SRR3934326 male single
+#qsub $script rhizoglyphus_robini SRR3934327 male single
+#qsub $script rhizoglyphus_robini SRR3934328 male single
+#qsub $script rhizoglyphus_robini SRR3934329 male single
+#qsub $script rhizoglyphus_robini SRR3934330 male single
+#qsub $script rhizoglyphus_robini SRR3934331 male single
+#qsub $script rhizoglyphus_robini SRR949074 male paired
+#qsub $script rhizoglyphus_robini SRR949075 male paired
+#qsub $script rhizoglyphus_robini SRR3934338 male single
+#qsub $script rhizoglyphus_robini SRR3934339 male single#
