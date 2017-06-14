f419f69e1c747b06e664ccd929a74afd50babd68
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-14 17:24:48 +0100

    .

--- scripts/master_download.sh
+++ scripts/master_download.sh
@@ -40,27 +40,31 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/download_q
 #qsub $script dendroctonus_ponderosae SRR867188 female paired
 #qsub $script dendroctonus_ponderosae SRR867432 male paired
 #qsub $script dendroctonus_ponderosae SRR867441 male paired
-qsub $script dendroctonus_ponderosae SRR867433 male paired
-qsub $script dendroctonus_ponderosae SRR867434 male paired
-#qsub $script dendroctonus_ponderosae SRR867436 male paired
-#qsub $script dendroctonus_ponderosae SRR867438 male paired
+#qsub $script dendroctonus_ponderosae SRR867433 male paired
+#qsub $script dendroctonus_ponderosae SRR867434 male paired
+qsub $script dendroctonus_ponderosae SRR867436 male paired
+qsub $script dendroctonus_ponderosae SRR867438 male paired
 #qsub $script dendroctonus_ponderosae SRR867439 male paired
 #qsub $script dendroctonus_ponderosae SRR867440 male paired
-#
-#qsub $script amblyomma_americanum SRR1027770 female paired  
-#qsub $script amblyomma_americanum SRR1027769 male paired 
-#qsub $script amblyomma_americanum SRR1027768 male paired 
-#qsub $script amblyomma_americanum SRR1027767 male paired 
-#qsub $script amblyomma_americanum SRR1027763 male paired 
-#qsub $script amblyomma_americanum SRR1027485 male paired 
-#qsub $script amblyomma_americanum SRR1027483 male paired 
-#
-#qsub $script tropilaelaps_mercedesae SRR4294671 female paired 
-#qsub $script tropilaelaps_mercedesae SRR4294670 female paired 
-#qsub $script tropilaelaps_mercedesae SRR4294669 male paired 
-#qsub $script tropilaelaps_mercedesae SRR4294668 male paired 
 
 
+#qsub $script rhizoglyphus_robini SRR3934324 female single
+#qsub $script rhizoglyphus_robini SRR3934325 female single
+#qsub $script rhizoglyphus_robini SRR3934332 female single
+#qsub $script rhizoglyphus_robini SRR3934333 female single
+#qsub $script rhizoglyphus_robini SRR3934334 female single
+#qsub $script rhizoglyphus_robini SRR3934335 female single
+#qsub $script rhizoglyphus_robini SRR3934336 female single
+#qsub $script rhizoglyphus_robini SRR3934337 female single
+#qsub $script rhizoglyphus_robini SRR946953 female paired
+#qsub $script rhizoglyphus_robini SRR3934326 male single
+#qsub $script rhizoglyphus_robini SRR3934327 male single
+#qsub $script rhizoglyphus_robini SRR3934328 male single
+#qsub $script rhizoglyphus_robini SRR3934329 male single
+#qsub $script rhizoglyphus_robini SRR3934330 male single
+#qsub $script rhizoglyphus_robini SRR3934331 male single
+#qsub $script rhizoglyphus_robini SRR3934338 male single
+#qsub $script rhizoglyphus_robini SRR3934339 male single
 
 
 
