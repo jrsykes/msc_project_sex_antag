a71b3d9579f19405890f50b5a8639d131431f7ab
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-20 22:25:46 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -2,25 +2,27 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-##### IMPORTANT. REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ########### for most this should be -l 100 and -s 20
+##### IMPORTANT. IF RUNNING SINGLE END MODE, REMMEBER TO LOOK AT SEQUENCE LENGTH IN MASTER_MAP.SH BFORE RUNNING ########### 
+######## for most libraries this should be -l 100 and -s 20 ##########
 
 
-qsub $script rhizoglyphus_robini SRR3934324 female single
-qsub $script rhizoglyphus_robini SRR3934325 female single
-qsub $script rhizoglyphus_robini SRR3934332 female single
-qsub $script rhizoglyphus_robini SRR3934333 female single
-qsub $script rhizoglyphus_robini SRR3934334 female single
-qsub $script rhizoglyphus_robini SRR3934335 female single
-qsub $script rhizoglyphus_robini SRR3934336 female single
-qsub $script rhizoglyphus_robini SRR3934337 female single
-qsub $script rhizoglyphus_robini SRR946953 female paired
-qsub $script rhizoglyphus_robini SRR3934326 male single
-qsub $script rhizoglyphus_robini SRR3934327 male single
-qsub $script rhizoglyphus_robini SRR3934328 male single
-qsub $script rhizoglyphus_robini SRR3934329 male single
-qsub $script rhizoglyphus_robini SRR3934330 male single
-qsub $script rhizoglyphus_robini SRR3934331 male single
-qsub $script rhizoglyphus_robini SRR949074 male paired
-qsub $script rhizoglyphus_robini SRR949075 male paired
-qsub $script rhizoglyphus_robini SRR3934338 male single
-qsub $script rhizoglyphus_robini SRR3934339 male single
+qsub $script planococcus_citric SRR1 female paired
+qsub $script planococcus_citric SRR2 male paired
+qsub $script planococcus_ficus SRR3 female paired
+qsub $script planococcus_ficus SRR4 male paired
+qsub $script dendroctonus_ponderosae SRR867160 female paired
+qsub $script dendroctonus_ponderosae SRR867161 female paired
+qsub $script dendroctonus_ponderosae SRR867162 female paired
+qsub $script dendroctonus_ponderosae SRR867179 female paired
+qsub $script dendroctonus_ponderosae SRR867176 female paired
+qsub $script dendroctonus_ponderosae SRR867183 female paired
+qsub $script dendroctonus_ponderosae SRR867186 female paired
+qsub $script dendroctonus_ponderosae SRR867188 female paired
+qsub $script dendroctonus_ponderosae SRR867432 male paired
+qsub $script dendroctonus_ponderosae SRR867441 male paired
+qsub $script dendroctonus_ponderosae SRR867433 male paired
+qsub $script dendroctonus_ponderosae SRR867434 male paired
+qsub $script dendroctonus_ponderosae SRR867436 male paired
+qsub $script dendroctonus_ponderosae SRR867438 male paired
+qsub $script dendroctonus_ponderosae SRR867439 male paired
+qsub $script dendroctonus_ponderosae SRR867440 male paired
