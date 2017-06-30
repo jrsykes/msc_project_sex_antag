cf031e1091b633afb7599edb0a5b3eadc9fc0a18
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-06-30 17:30:59 +0100

    .

--- scripts/master_map.sh
+++ scripts/master_map.sh
@@ -13,16 +13,16 @@ script=/data/projects/lross_ssa/scripts/msc_project_sex_antag/scripts/map.sh
 
 ### 1 =species. 2 = SRR, 3 = sex, 4 = paired/single end mode
 
-qsub $script mayetiola_destructor SRR1738673 male paired   
-qsub $script mayetiola_destructor SRR1239358 male paired   
-qsub $script mayetiola_destructor SRR1239355 male paired   
-qsub $script mayetiola_destructor SRR1738672 female paired   
-qsub $script mayetiola_destructor SRR1239356 female paired   
-qsub $script mayetiola_destructor SRR1239354 female paired   
-#sub $script planococcus_pooled SRR1 female paired  
-#sub $script planococcus_pooled SRR3 female paired  
-#sub $script planococcus_pooled SRR4 male paired 
-#sub $script planococcus_pooled SRR2 male paired 
+#qsub $script mayetiola_destructor SRR1738673 male paired   
+#qsub $script mayetiola_destructor SRR1239358 male paired   
+#qsub $script mayetiola_destructor SRR1239355 male paired   
+#qsub $script mayetiola_destructor SRR1738672 female paired   
+#qsub $script mayetiola_destructor SRR1239356 female paired   
+#qsub $script mayetiola_destructor SRR1239354 female paired   
+sub $script planococcus_pooled SRR1 female paired  
+sub $script planococcus_pooled SRR3 female paired  
+sub $script planococcus_pooled SRR4 male paired 
+sub $script planococcus_pooled SRR2 male paired 
 #qsub $script 
 #qsub $script 
 #qsub $script 
