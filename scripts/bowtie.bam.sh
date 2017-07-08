6f3fe2207bb132296183db6d31d6bb156eef02ab
jrsykes <jamiesykes1991@hotmail.co.uk>
2017-07-08 15:53:23 +0100

    .

--- scripts/bowtie.bam.sh
+++ scripts/bowtie.bam.sh
@@ -17,16 +17,20 @@ SPECIES=anoplophora_glabripennis
 SRR1=SRR1799851
 SRR2=SRR1799852
 
-PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR1\ --thread_count 16
+mkdir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir
+mkdir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR1
+mkdir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR2
+
+PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_1.fq --right //data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_1.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR1 --thread_count 16
 PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR2\ --thread_count 16
 
 
-SPECIES=leptinotarsa_decemlineata
-SRR1=SRR1827565
-SRR2=SRR1827566
-
-PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR1\ --thread_count 16
-PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR2\ --thread_count 16
+#SPECIES=leptinotarsa_decemlineata
+#SRR1=SRR1827565
+#SRR2=SRR1827566
+#
+#PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/female/$SRR1\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR1\ --thread_count 16
+#PATH=$PATH:/exports/software/bowtie/bowtie-1.2-legacy:/exports/software/rsem/RSEM-1.3.0:/exports/software/samtools/samtools-1.1; /exports/software/trinity/trinityrnaseq-Trinity-v2.4.0/util/align_and_estimate_abundance.pl --transcripts /data/projects/lross_ssa/analyses/$SPECIES/trinity/Trinity1k.fasta --seqType fq --left /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_1.fq --right /data/projects/lross_ssa/analyses/$SPECIES/trimmomatic/male/$SRR2\_2.fq --est_method RSEM --aln_method bowtie --trinity_mode --coordsort_bam --prep_reference --output_dir /data/projects/lross_ssa/analyses/$SPECIES/rsem_outdir/$SRR2\ --thread_count 16
 
 #SPECIES=pachypsylla_venusta
 #SRR1=SRR1824579
